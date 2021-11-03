848c77ae9e360b478cc372bb398f6f6b5eec96c0
spockinius <93668858+spockinius@users.noreply.github.com>
2021-11-03 20:26:31 +0200

    tehtäviä 031121

--- Ohjelmoinnin perusteet/Yksinkertainen silmukka_esimerkkejä.py
+++ Ohjelmoinnin perusteet/Yksinkertainen silmukka_esimerkkejä.py
@@ -18,5 +18,50 @@ while True:
         print(sqrt(luku))
 
     elif luku == 0:
-        print("Lopetetaan...")  
+        print("Lopetetaan...")
         break          
+
+# Lähtölaskenta
+luku = 5
+print("Lähtölaskenta!")
+while True:     
+    print(luku)
+
+    luku = luku - 1
+
+    if luku == 0:
+        break
+       
+print("Nyt!")       
+
+# Salasanan syöttäminen
+salasana = input("Salasana: ")
+while True:
+    toista = input("Toista salasana: ")
+
+    if toista != salasana:
+        print("Ei ollut sama!")
+    
+    elif salasana == salasana:
+        break
+    
+print("Käyttäjätunnus luotu!")
+
+# Kysy pin-koodi, laske yritykset
+yritykset = 1
+koodi = 4321
+while True:
+    koodi = int(input("PIN-koodi: "))
+    
+    if koodi == 4321:
+        if yritykset == 1:
+            print("Oikein, tarvitsit vain yhden yrityksen!")
+            break
+
+        if yritykset > 1:
+            print(f"Oikein, tarvitsit {yritykset} yritystä")
+            break
+    
+    if koodi != 4321:
+        print("Väärin")
+        yritykset = yritykset + 1
